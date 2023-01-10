727436aca553738e07c6662ae7a960790b27ab5d
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-01-10 18:08:05 +0300

    Add component folder

---
+++ src/components/JobList.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const JobList = () => {
+  return (
+    <div>
+      <div className="company-logo">
+        <img src="" alt="company logo" />
+      </div>
+      <div className="compay-role">
+        <div className="company-role-tile">
+          <p className="job-title">Photosnap</p>
+          <button className="new">NEW!</button>
+          <button className="featured">FEATURED</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default JobList;
