c39455a053dd212490408758072ea7a98561894d
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-01-15 13:53:36 +0300

    semantic html

--- src/components/JobList.tsx
+++ src/components/JobList.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Role } from "../model";
-// import Img from "../images/photosnap.svg";
-
+import "./JobList.css";
 interface Props {
   role: Role;
 }
@@ -10,27 +9,36 @@ const JobList = ({ role }: Props) => {
   console.log(role.logo);
 
   return (
-    <div>
-      <div className="company-logo">
-        <img src={role.logo} alt="company logo" />
-        <div className="job-description">
-          <div className="company-name">
-            <p className="company-name">{role.company}</p>
-            <button className="new">NEW!</button>
-            <button className="featured">FEATURED</button>
+    <section className="job-list">
+      <div className="container">
+        <div className="role">
+          <div className="company-logo">
+            <img src={role.logo} alt="company logo" />
           </div>
-
-          <div>Senior frontend developer</div>
-          <div className="job-type">
-            <span>1day</span>
-            <span className="dot"></span>
-            <span>1day</span>
-            <span className="dot"></span>
-            <span>1day</span>
+          <div className="role-description">
+            <div className="company-description flex">
+              <p className="company-name">{role.company}</p>
+              <div className="job-status">
+                <button className="new">NEW!</button>
+                <button className="featured">FEATURED</button>
+              </div>
+            </div>
+            <div className="position">
+              <h4 className="position__name">{role.position}</h4>
+            </div>
+            <div className="job-type flex">
+              <span>{role.postedAt}</span>
+              <span className="dot"></span>
+              <span>{role.contract}</span>
+              <span className="dot"></span>
+              <span>{role.location}</span>
+            </div>
           </div>
         </div>
+
+        <div className="languages"></div>
       </div>
-    </div>
+    </section>
   );
 };
 
