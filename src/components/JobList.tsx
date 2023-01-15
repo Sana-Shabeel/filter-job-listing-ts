1427cc841b0c9d746abdc9fbb1f9a2217d2c3042
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-01-15 03:50:06 +0300

    fix img not showing

--- src/components/JobList.tsx
+++ src/components/JobList.tsx
@@ -1,30 +1,33 @@
 import React from "react";
 import { Role } from "../model";
+// import Img from "../images/photosnap.svg";
 
 interface Props {
   role: Role;
 }
 
 const JobList = ({ role }: Props) => {
+  console.log(role.logo);
+
   return (
     <div>
       <div className="company-logo">
         <img src={role.logo} alt="company logo" />
-      </div>
-      <div className="job-description">
-        <div className="company-name">
-          <p className="company-name">Photosnap</p>
-          <button className="new">NEW!</button>
-          <button className="featured">FEATURED</button>
-        </div>
+        <div className="job-description">
+          <div className="company-name">
+            <p className="company-name">{role.company}</p>
+            <button className="new">NEW!</button>
+            <button className="featured">FEATURED</button>
+          </div>
 
-        <div>Senior frontend developer</div>
-        <div className="job-type">
-          <span>1day</span>
-          <span className="dot"></span>
-          <span>1day</span>
-          <span className="dot"></span>
-          <span>1day</span>
+          <div>Senior frontend developer</div>
+          <div className="job-type">
+            <span>1day</span>
+            <span className="dot"></span>
+            <span>1day</span>
+            <span className="dot"></span>
+            <span>1day</span>
+          </div>
         </div>
       </div>
     </div>
