db8dad45ac6a5ecce3d885a61686a21bd6c52373
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-01-18 05:26:07 +0300

    Handle click function

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import data from "./data.json";
+import { Role } from "./model";
 import "./App.css";
 import JobList from "./components/JobList";
+import Tab from "./components/Tab";
 
 function App() {
   // console.log(data);
 
+  const [job, setJob] = useState<Role[]>(data);
+  const [filter, setFilter] = useState<string[]>([]);
+
+  const handleClick = (lang: string) => {
+    const res = data.filter((d) => d.languages.includes(lang));
+    setFilter((prev) => [...prev, lang]);
+    setJob(res);
+
+    console.log(res);
+  };
+
   return (
     <div className="App">
       <header className="App-header"></header>
-      {data.map((role) => (
-        <JobList role={role} key={role.id} />
+      <Tab filter={filter} />
+      {job.map((role) => (
+        <JobList role={role} key={role.id} handleClick={handleClick} />
       ))}
     </div>
   );
