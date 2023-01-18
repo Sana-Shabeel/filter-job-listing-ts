5f9f540f3014ef39a4cea1b3311a4bbb0d0b4889
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-01-19 02:17:52 +0300

    add handleRemove func

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -3,11 +3,12 @@ import { ImCross } from "react-icons/im";
 
 interface Props {
   filter: string[];
+  setFilter: React.Dispatch<React.SetStateAction<string[]>>;
 }
 
-const Tab = ({ filter }: Props) => {
-  const handleRemove = (e: React.MouseEvent) => {
-    console.log(e.currentTarget.parentElement?.classList.toggle("remove"));
+const Tab = ({ filter, setFilter }: Props) => {
+  const handleRemove = (e: React.MouseEvent, idx: number) => {
+    setFilter((prev) => prev.filter((_, i) => i !== idx));
   };
 
   return (
@@ -16,7 +17,7 @@ const Tab = ({ filter }: Props) => {
         <div className="tab--btn" key={i}>
           {" "}
           <span className="tab--btn__text">{lang}</span>
-          <span className="tab--btn__cross" onClick={(e) => handleRemove(e)}>
+          <span className="tab--btn__cross" onClick={(e) => handleRemove(e, i)}>
             <ImCross />
           </span>
         </div>
